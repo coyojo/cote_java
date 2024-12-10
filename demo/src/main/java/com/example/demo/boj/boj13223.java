382b5cbaca12ac3817f43c44cc34e8c7c938084a
YOUNJAE <coyojo@naver.com>
2024-12-10 16:54:58 +0900

    13223번 풀이

---
+++ demo/src/main/java/com/example/demo/boj/boj13223.java
@@ -0,0 +1,20 @@
+package com.example.demo.boj;
+
+public class boj13223 {
+    public static void main(String[] args){
+        //HH:MM:SS 포맷의 두 시각의 차이를 HH:MM:SS 포맷으로 출력하기
+        /*
+
+         1. ' : ' 문자를 기준으로 시간, 분, 초를 쪼갠다.
+             각 단위의 인덱스에서 10의 자리와 1의 자리를 구해 계산할 수 있다.
+             String time = "09:10:59";
+             int hour = (time.charAt(0) - '0') * 10 + time.charAt(1)-'0';
+             위 코드를 자세히 살펴보자면, String으로 받은 값이기 때문에 int로 바꾸기 위해서 아스키 값인 '0'을 빼줘야 int로 바꿀 수 있다.
+         2. 두 시간, 분, 초의 차이를 계산한다.
+         3. 구해진 시간을 HH:MM:SS 형태로 출력
+
+         */
+
+
+    }
+}
