1ac32adb878162e6df358be83b486ab247690f9a
YOUNJAE <coyojo@naver.com>
2025-01-03 14:01:13 +0900

    15552번 문제
    BufferedReader

---
+++ demo/src/main/java/com/example/demo/boj/boj15552.java
@@ -0,0 +1,18 @@
+package com.example.demo.boj;
+
+import java.io.*;
+
+public class boj15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0){
+            String[] nums = br.readLine().split(" ");
+            int a = Integer.parseInt(nums[0]);
+            int b = Integer.parseInt(nums[1]);
+            bw.write(a+b+"\n");
+        }
+        bw.flush();
+    }
+}
